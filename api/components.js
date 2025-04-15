32fe07337ea814671112e93c92012670b44d304f
bloqxstudio <bloqxcompany@gmail.com>
2025-04-15 19:57:02 -0300

    Update components.js

--- api/components.js
+++ api/components.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   try {
     const response = await fetch("https://amruhpzfiaqskvtssfqi.supabase.co/rest/v1/components", {
       headers: {
-        apikey: process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtcnVocHpmaWFxc2t2dHNzZnFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5NDMzODQsImV4cCI6MjA1ODUxOTM4NH0.pbSMQ3QfUlqXlIA_UVnSuEWc6PIPdoT2kFMpUzl94-4
+        apikey: process.env.SUPABASE_API_KEY
       }
     });
 
